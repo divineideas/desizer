f5229db1befbcb2f011fe697521d7d0641cac9a2
domoconnell <dom@divineideasgroup.com>
2014-02-07 09:59:33 +0000

    stuffs

--- template.js
+++ template.js
@@ -22,14 +22,11 @@
 					var re = new RegExp("{{{" + i + "}}}","gi");
 					that.html = that.html.replace (re, val);
 					var rehtml = new RegExp("{{" + i + "}}","gi");
-					htmlval = htmlEntities(val);
+					htmlval = String(val).replace(/<\/?[^>]+(>|$)/g, "");;
 					that.html = that.html.replace (rehtml, htmlval);
 				});
 			}
 			return this.html;
-		},
-		htmlEntities: function(str){
-	   		return String(str).replace(/<\/?[^>]+(>|$)/g, "");
 		}
 	}
 })(window, document);
